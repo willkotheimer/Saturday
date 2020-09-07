72c56068db1160eb070ab58dae49d620aa2adff6
wkotheimer <wkotheimer@gmail.com>
2020-09-06 22:19:30 -0500

    Adding Saturday

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').html('<h1>HELLO! Saturday!</h1>');
+  console.log('YOU ARE UP ITS saTURDAY!');
 };
 
 init();
